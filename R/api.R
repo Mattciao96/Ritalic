bb121324ee05ef10cbf3b64ba5cba4930ec2ae50
Mattciao96 <matt.ciao@gmail.com>
2022-10-28 14:16:15 +0200

    initial commit

---
+++ R/api.R
@@ -0,0 +1,27 @@
+library(httr)
+library(jsonlite)
+library(dplyr)
+library(sp)
+library(rgdal)
+library(raster)
+
+
+getLicPoints <- function(sp_name){
+  res <- GET('https://italic.units.it/?procedure=occurrencedata', query = list(sp = sp_name))
+  data = fromJSON(rawToChar(res$content))
+  data <- bind_rows(data, .id = "herbarium")
+  return(data)
+}
+
+dfToSpatialPoints <- function(getLicPointsOutput){
+  points <- getLicPointsOutput
+  points$lat <- suppressWarnings(as.numeric(points$lat))
+  points$long <- suppressWarnings(as.numeric(points$long))
+  points <- na.omit(points)
+  coordinates(points)<-~long+lat
+  crs(points) <- CRS("+proj=longlat +datum=WGS84")
+  return(points)
+}
+
+
+
